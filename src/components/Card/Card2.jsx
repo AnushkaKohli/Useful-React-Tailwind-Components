fd0acc80477d3f0a106cc878a66a1967e84e3696
AnushkaKohli <102284552+AnushkaKohli@users.noreply.github.com>
2023-08-04 21:28:18 +0530

    finished card2

--- src/components/Card/Card2.jsx
+++ src/components/Card/Card2.jsx
@@ -2,37 +2,40 @@ import React from "react";
 import { FaShoppingCart, FaBookmark, FaStar, FaFireAlt } from "react-icons/fa";
 
 const Card2 = () => {
-  // var rating = 4;
+  var rating = 3;
   return (
     <div className="grid grid-cols-3 justify-center">
-      <div className="flex justify-evenly mt-20">
-        <div className="relative bg-gray-100 m-1 grow shrink-0 basos-96 max-w-lg max-h-[50rem] duration-200 hover:shadow-[0.5rem 0.75rem 1.5rem #bbbbbb]">
-          <img className="mt-8 max-w-full h-auto"
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcR90pZoWX_UY9f1QPYuGj5nM9IdqyahZViE-4xFJkwsQLwCalJmio--68GY2FhCaZNugGfNq_CkGU02HpkPr-UC3LL6JEr3SCBRKP105eSsNfO5mdqS9g4vQg&usqp=CAE"
-            alt="product"
-          />
+      <div className="flex mt-20">
+        <div className="relative flex justify-items-start flex-col bg-gray-100 m-1 grow shrink-0 base-96 max-w-lg max-h-[50rem] duration-200 hover:-translate-y-2 hover:shadow-[0.5rem 0.75rem 1.5rem #bbbbbb]">
+            <img
+              className="mt-8 max-w-fit max-h-80 h-auto"
+              alt="product"
+              src="https://m.media-amazon.com/images/I/71ne0uMgcgL._SX466_.jpg"
+            />
 
-          <FaShoppingCart className="absolute right-0 m-4 cursor-pointer" />
-          <FaBookmark className="absolute right-12 m-4 cursor-pointer" />
-          <FaFireAlt className="absolute left-0 m-4 cursor-pointer" />
+          <div>
+            <FaShoppingCart className="absolute right-0 m-4 cursor-pointer" />
+            <FaBookmark className="absolute right-8 m-4 cursor-pointer" />
+            {/* <FaFireAlt className="absolute left-0 m-4 cursor-pointer" /> */}
+          </div>
 
-          <div className="m-8">
-            <h3>BoAt Rockerz 370</h3>
-            <div>
-              <div>₹999.00</div>
-              <div>300 units sold</div>
+          <div className="m-8 items-center">
+            <h3 className="text-2xl">BoAt Rockerz 370</h3>
+            <div className="my-8 flex justify-between items-center">
+              <div className="text-2xl font-bold">₹999.00</div>
+              <div className="text-[1.1rem]">300 units sold</div>
             </div>
-            <div>
-              <div>
-                {/* {[...Array(rating)].map((index) => (
-                            <FaStar id={index + 1} key={index} />
-                        ))} */}
+            <div className="my-8 flex justify-between items-center">
+              <div className="flex flex-row gap-1">
+                {[...Array(rating)].map((index) => (
+                            <FaStar key={index} />
+                        ))}
+                {/* <FaStar />
                 <FaStar />
                 <FaStar />
-                <FaStar />
-                <FaStar />
+                <FaStar /> */}
               </div>
-              <div>10 left</div>
+              <div className="text-[1.1rem]">10 left</div>
             </div>
           </div>
         </div>
